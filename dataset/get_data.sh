b7c108924c856f97f11e5c61b6a682b42eb6b637
Aaditya GPGPU <aprakash@brandeis.edu>
2016-04-04 02:10:28 -0400

    added the sub-module for NTM, which only implements copy thus requires some work inorder to port it to VQA

---
+++ dataset/get_data.sh
@@ -0,0 +1,19 @@
+
+mkdir -p mscoco/images
+cd MSCOCO/images
+# trainset images
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+# valset images
+wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+# testset images
+wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip
+# unzip
+unzip train2014.zip
+unzip val2014.zip
+unzip test2015.zip
+# rm compressed files
+rm -rf train2014.zip
+rm -rf val2014.zip
+rm -rf test2015.zip
+cd ../..
+
